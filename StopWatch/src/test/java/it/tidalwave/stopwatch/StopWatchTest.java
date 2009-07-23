b0063a4b656877f03b13ef96cb1e95f1ade91ec9
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-07-23 09:30:00 +0200

    Added dummy test.

---
+++ StopWatch/src/test/java/it/tidalwave/stopwatch/StopWatchTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.tidalwave.stopwatch;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author fritz
+ */
+public class StopWatchTest
+  {
+    @Test
+    public void dummy()
+      {
+      }
+  }
