1200dc2971a206b9d74d935c841be857ccc3f02a
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2010-02-17 14:01:38 +0100

    Fixed copyright header.

--- SingleLineLogFormatter/src/main/java/it/tidalwave/util/logging/SingleLineLogFormatter.java
+++ SingleLineLogFormatter/src/main/java/it/tidalwave/util/logging/SingleLineLogFormatter.java
@@ -1,30 +1,27 @@
-/*******************************************************************************
+/***********************************************************************************************************************
  *
- * OpenBlueSky - Enhanced NetBeans RCP Components
- * =====================================================
+ * TheseFoolishThings - Miscellaneous utilities
+ * ============================================
  *
- * Copyright (C) 2003-2009 by Fabrizio Giudici
- * Project home page: http://openbluesky.dev.java.net
+ * Copyright (C) 2009-2010 by Tidalwave s.a.s.
+ * Project home page: http://thesefoolishthings.kenai.com
  *
- *******************************************************************************
+ ***********************************************************************************************************************
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *******************************************************************************
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
  *
  * $Id$
  *
- ******************************************************************************/
+ **********************************************************************************************************************/
 package it.tidalwave.util.logging;
 
 import java.io.PrintWriter;
