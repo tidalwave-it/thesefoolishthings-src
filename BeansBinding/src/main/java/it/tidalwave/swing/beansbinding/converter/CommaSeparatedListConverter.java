bba4341d93f5f860430d8d9392bfbde5bdaf44a4
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2010-02-17 16:21:03 +0100

    Fix for THESEFOOLISHTHINGS-3.

---
+++ BeansBinding/src/main/java/it/tidalwave/swing/beansbinding/converter/CommaSeparatedListConverter.java
@@ -0,0 +1,87 @@
+/***********************************************************************************************************************
+ *
+ * TheseFoolishThings - Miscellaneous utilities
+ * ============================================
+ *
+ * Copyright (C) 2009-2010 by Tidalwave s.a.s.
+ * Project home page: http://thesefoolishthings.kenai.com
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * $Id$
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.swing.beansbinding.converter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.jdesktop.beansbinding.Converter;
+
+/*******************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ ******************************************************************************/
+public class CommaSeparatedListConverter extends Converter<Collection<String>, String>
+  {
+    @Override
+    public String convertForward (final Collection<String> collection) 
+      {
+        if (collection == null)
+          {
+            return null;
+          }
+        
+        final StringBuilder builder = new StringBuilder();
+        
+        for (final String s : collection)
+          {
+            if (builder.length() > 0)
+              {
+                builder.append(", ");  
+              }
+            
+            builder.append(s);
+          }
+        
+        return builder.toString();
+      }
+
+    @Override
+    public Collection<String> convertReverse (final String string) 
+      {
+        if (string == null)
+          {
+            return null;
+          }
+        
+        if (string.trim().equals(""))
+          {
+            return Collections.<String>emptyList();  
+          }
+        
+        final List<String> result = new ArrayList<String>();
+        final String[] split = string.split(",");
+        
+        for (final String s : split)
+          {
+            result.add(s.trim());
+          } 
+        
+        return result;
+      }
+  }
---
+++ BeansBinding/src/test/java/it/tidalwave/swing/beansbinding/converter/CommaSeparatedListConverterTest.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ *
+ * OpenBlueSky - Enhanced NetBeans RCP Components
+ * ==============================================
+ *
+ * Copyright (C) 2003-2009 by Fabrizio Giudici
+ * Project home page: http://openbluesky.dev.java.net
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ *******************************************************************************
+ *
+ * $Id$
+ *
+ ******************************************************************************/
+package it.tidalwave.swing.beansbinding.converter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*******************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ ******************************************************************************/
+public class CommaSeparatedListConverterTest 
+  {
+    private CommaSeparatedListConverter converter;
+
+    @Before
+    public void setUp() 
+      {
+        converter = new CommaSeparatedListConverter();
+      }  
+
+    @After
+    public void tearDown() 
+      {
+        converter = null;
+      }
+
+    @Test
+    public void convertForward1() 
+      {
+        assertEquals("one", converter.convertForward(Arrays.asList("one")));
+      }
+
+    @Test
+    public void convertForward2() 
+      {
+        assertEquals("one, two", converter.convertForward(Arrays.asList("one", "two")));
+      }
+
+    @Test
+    public void convertForwardNull() 
+      {
+        assertEquals(null, converter.convertForward(null));
+      }
+
+    @Test
+    public void convertForwardEmpty() 
+      {
+        assertEquals("", converter.convertForward(Collections.<String>emptyList()));
+      }
+    
+    @Test
+    public void convertReverse1() 
+      {
+        assertEquals(Arrays.asList("one"), converter.convertReverse("one"));
+      }
+    
+    @Test
+    public void convertReverse2() 
+      {
+        assertEquals(Arrays.asList("one", "two"), converter.convertReverse("one, two"));
+      }
+    
+    @Test
+    public void convertReverseNull() 
+      {
+        assertEquals(null, converter.convertReverse(null));
+      }
+    
+    @Test
+    public void convertReverseEmpty() 
+      {
+        assertEquals(Collections.<String>emptyList(), converter.convertReverse(""));
+      }
+  }
