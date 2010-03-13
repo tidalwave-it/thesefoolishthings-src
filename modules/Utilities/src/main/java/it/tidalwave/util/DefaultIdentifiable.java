03ba1c03f6b70cc282bb6076d7007836561c34a1
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2010-03-13 22:06:31 +0100

    Fix for THESEFOOLISHTHINGS-10.

---
+++ modules/Utilities/src/main/java/it/tidalwave/util/DefaultIdentifiable.java
@@ -0,0 +1,56 @@
+/***********************************************************************************************************************
+ *
+ * TheseFoolishThings - Miscellaneous utilities
+ * ============================================
+ *
+ * Copyright (C) 2009-2010 by Tidalwave s.a.s.
+ * Project home page: http://thesefoolishthings.kenai.com
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * $Id$
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.util;
+
+import javax.annotation.Nonnull;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class DefaultIdentifiable implements Identifiable
+  {
+    @Nonnull
+    private final Id id;
+
+    public DefaultIdentifiable (final @Nonnull Id id)
+      {
+        this.id = id;
+      }
+
+    @Override @Nonnull
+    public Id getId()
+      {
+        return id;
+      }
+
+    @Override @Nonnull
+    public String toString()
+      {
+        return String.format("DefaultIdentifiable[%s]", id);
+      }
+  }
