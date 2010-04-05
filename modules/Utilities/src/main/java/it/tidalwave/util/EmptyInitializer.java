753ea792871d4c6668dd1cb61d93c067f413aa45
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2010-04-05 18:22:22 +0200

    Initializer, EmptyInitializer, Task moved from OpenBlueSky.

---
+++ modules/Utilities/src/main/java/it/tidalwave/util/EmptyInitializer.java
@@ -0,0 +1,48 @@
+/***********************************************************************************************************************
+ *
+ * OpenBlueSky - NetBeans Platform Enhancements
+ * ============================================
+ *
+ * Copyright (C) 2007-2010 by Tidalwave s.a.s.
+ * Project home page: http://openbluesky.kenai.com
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * $Id$
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.util;
+
+import javax.annotation.Nonnull;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ * @draft
+ *
+ **********************************************************************************************************************/
+public final class EmptyInitializer<K> implements Initializer<K>
+  {
+    public static <K> Initializer<K> instance()
+      {
+        return new EmptyInitializer<K>();   
+      }
+
+    @Nonnull @Override
+    public K initialize (@Nonnull final K entity)
+      {
+        return entity;
+      }
+  }
