6199313b01adf9c6f21a72ecb7a8774b0a8522c9
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-02-28 15:48:35 +0100

    Fixed headers.

--- modules/BeansBinding/src/test/java/it/tidalwave/swing/beansbinding/converter/CommaSeparatedListConverterTest.java
+++ modules/BeansBinding/src/test/java/it/tidalwave/swing/beansbinding/converter/CommaSeparatedListConverterTest.java
@@ -1,30 +1,26 @@
-/*******************************************************************************
+/***********************************************************************************************************************
  *
- * OpenBlueSky - Enhanced NetBeans RCP Components
- * ==============================================
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2011 by Tidalwave s.a.s.
  *
- * Copyright (C) 2003-2009 by Fabrizio Giudici
- * Project home page: http://openbluesky.dev.java.net
+ ***********************************************************************************************************************
  *
- *******************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *******************************************************************************
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.kenai.com
+ * SCM: https://kenai.com/hg/thesefoolishthings~src
  * $Id$
  *
- ******************************************************************************/
+ **********************************************************************************************************************/
 package it.tidalwave.swing.beansbinding.converter;
 
 import java.util.Arrays;
