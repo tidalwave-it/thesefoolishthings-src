7cd0fca5f1a6103a84096ee6af2484280ba883b9
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-03-02 19:13:34 +0100

    Added IO roles.

---
+++ modules/Utilities/src/main/java/it/tidalwave/role/io/FileBinaryWritable.java
@@ -0,0 +1,53 @@
+/***********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2011 by Tidalwave s.a.s.
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.kenai.com
+ * SCM: https://kenai.com/hg/thesefoolishthings~src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.role.io;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/***********************************************************************************************************************
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class FileBinaryWritable implements BinaryWritable
+  {
+    @Nonnull
+    private final File file;
+
+    public FileBinaryWritable (final @Nonnull File file) 
+      {
+        this.file = file;
+      }
+
+    @Override @Nonnull
+    public OutputStream openStream() 
+      throws IOException 
+      {
+        return new FileOutputStream(file);
+      }
+  }
