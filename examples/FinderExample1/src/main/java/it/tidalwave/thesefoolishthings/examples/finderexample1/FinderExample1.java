671c38ec214afb1eb1173f376e8b47297261fcd4
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-03-06 02:21:23 +0100

    Added runnable main().

---
+++ examples/FinderExample1/src/main/java/it/tidalwave/thesefoolishthings/examples/finderexample1/FinderExample1.java
@@ -0,0 +1,67 @@
+/***********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.kenai.com
+ * SCM: https://kenai.com/hg/thesefoolishthings~src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.thesefoolishthings.examples.finderexample1;
+
+import javax.annotation.Nonnull;
+import it.tidalwave.util.NotFoundException;
+import static it.tidalwave.thesefoolishthings.examples.finderexample1.PersonSortCriterion.*;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class FinderExample1
+  {
+    public static void main (final @Nonnull String ... args)
+      throws NotFoundException 
+      {
+        final PersonRegistry registry = new DefaultPersonRegistry();
+
+        registry.add(new Person("Richard", "Nixon"));
+        registry.add(new Person("Jimmy", "Carter"));
+        registry.add(new Person("Ronald", "Reagan"));
+        registry.add(new Person("George", "Bush"));
+        registry.add(new Person("Bill", "Clinton"));
+        registry.add(new Person("George Walker", "Bush"));
+        registry.add(new Person("Barack", "Obama"));
+        
+        //@bluebook-begin example
+        System.out.println("All: " 
+                           + registry.findPersons().results());
+        
+        System.out.println("Two persons from the 3rd position: " 
+                           + registry.findPersons().from(3).max(2).results());
+        
+        System.out.println("Whose first name starts with B: " 
+                           + registry.findPersons().withFirstName("B.*").results());
+        
+        System.out.println("Whose first name starts with B, sorted by last name: " 
+                           + registry.findPersons().withFirstName("B.*").sort(BY_FIRST_NAME).results());
+        
+        System.out.println("The first found whose last name is Bush: "
+                           + registry.findPersons().withLastName("Bush").firstResult());
+        //@bluebook-end example
+     }
+  }
