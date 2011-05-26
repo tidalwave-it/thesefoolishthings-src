4424955281c6870bd02c301b7b7b1e63d1da1954
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-05-26 16:50:39 +0200

    Fix for THESEFOOLISHTHINGS-47.

---
+++ modules/Utilities/src/main/java/it/tidalwave/role/ui/Presentable.java
@@ -0,0 +1,43 @@
+/***********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.kenai.com
+ * SCM: https://kenai.com/hg/thesefoolishthings~src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.role.ui;
+
+import javax.annotation.Nonnull;
+
+/***********************************************************************************************************************
+ * 
+ * The role of an object that can be presented on a UI, thus is capable of creating a {@link PresentationModel}.
+ * 
+ * @stereotype Role
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public interface Presentable 
+  {
+    public final static Class<Presentable> Presentable = Presentable.class;
+    
+    @Nonnull
+    public PresentationModel createPresentationModel (@Nonnull Object... instanceCapabilities);
+  }
