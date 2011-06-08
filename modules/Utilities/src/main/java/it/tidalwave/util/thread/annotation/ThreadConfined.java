fdcd06612a82d2da29ac18c3a74ab2f6b8314d4b
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-06-09 00:41:46 +0200

    Fix for THESEFOOLISHTHINGS-54.

---
+++ modules/Utilities/src/main/java/it/tidalwave/util/thread/annotation/ThreadConfined.java
@@ -0,0 +1,46 @@
+/***********************************************************************************************************************
+ *
+ * blueBill Core - open source birding
+ * Copyright (C) 2009-2011 by Tidalwave s.a.s. (http://www.tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://bluebill.tidalwave.it
+ * SCM: https://kenai.com/hg/bluebill~core-src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.util.thread.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import it.tidalwave.util.thread.ThreadType;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
+@Inherited
+@Documented
+public @interface ThreadConfined
+  {
+    ThreadType type();
+  }
