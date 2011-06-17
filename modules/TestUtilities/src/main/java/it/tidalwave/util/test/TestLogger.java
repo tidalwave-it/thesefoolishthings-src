93173e0c5ff3528e8b01836648adb934181e2217
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2011-06-17 23:55:01 +0200

    Fixed test logging.

--- modules/TestUtilities/src/main/java/it/tidalwave/util/test/TestLogger.java
+++ modules/TestUtilities/src/main/java/it/tidalwave/util/test/TestLogger.java
@@ -22,6 +22,7 @@
  **********************************************************************************************************************/
 package it.tidalwave.util.test;
 
+import java.util.Arrays;
 import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,35 +37,57 @@ import org.testng.TestListenerAdapter;
  **********************************************************************************************************************/
 public class TestLogger extends TestListenerAdapter
   {
-    private static final Logger log = LoggerFactory.getLogger(TestLogger.class);
-    
-    private static final String SEPARATOR = "************************************************************************************************";
+    private static final String S = "***********************************************";
+    private static final String SEPARATOR = S + S + S + S;
     
     @Override
     public void onTestStart (final @Nonnull ITestResult result) 
       {
-        final String separator = SEPARATOR.substring(0, Math.min(result.getName().length() + 7, SEPARATOR.length()));
-        log.info("");
+        String args = "";
+        
+        final Object[] parameters = result.getParameters();
+        
+        if ((parameters != null) && parameters.length > 0)
+          {
+            args = Arrays.toString(parameters);
+          }
+        
+        final String separator = SEPARATOR.substring(0, Math.min(Math.max(args.length() + 5, result.getName().length() + 7), SEPARATOR.length()));
+        final Logger log = LoggerFactory.getLogger(result.getTestClass().getRealClass());
+    
         log.info(separator);
         log.info("TEST \"{}\"", result.getName().replace('_', ' '));
+        
+        if (!"".equals(args))
+          {
+            log.info("ARGS {}", args);
+          }
+        
         log.info(separator);
       }
 
     @Override
     public void onTestFailure (final @Nonnull ITestResult result) 
       {
-        log.info("TEST FAILED");
+        final Logger log = LoggerFactory.getLogger(result.getTestClass().getRealClass());
+        log.info("TEST FAILED in {} msec - {}", result.getEndMillis() - result.getStartMillis(), result.getThrowable());
+        log.info("");
       }
 
     @Override
     public void onTestSkipped (final @Nonnull ITestResult result) 
       {
+        onTestStart(result);
+        final Logger log = LoggerFactory.getLogger(result.getTestClass().getRealClass());
         log.info("TEST SKIPPED");
+        log.info("");
       }
 
     @Override
     public void onTestSuccess (final @Nonnull ITestResult result)
       {
-        log.info("TEST PASSED");
+        final Logger log = LoggerFactory.getLogger(result.getTestClass().getRealClass());
+        log.info("TEST PASSED in {} msec", result.getEndMillis() - result.getStartMillis());
+        log.info("");
       }
   } 
