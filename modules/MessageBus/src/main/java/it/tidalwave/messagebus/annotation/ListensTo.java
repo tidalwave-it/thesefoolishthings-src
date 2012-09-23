8cd1b07d97f07e25c7d17cfc71b83524f12c13f7
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2012-09-23 22:08:08 +0200

    ...

--- modules/MessageBus/src/main/java/it/tidalwave/messagebus/annotation/ListensTo.java
+++ modules/MessageBus/src/main/java/it/tidalwave/messagebus/annotation/ListensTo.java
@@ -22,12 +22,12 @@
  **********************************************************************************************************************/
 package it.tidalwave.messagebus.annotation;
 
-import it.tidalwave.messagebus.MessageBusHelper;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import it.tidalwave.messagebus.MessageBusHelper;
 
 /***********************************************************************************************************************
  * 
