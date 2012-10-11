18c6524354cefe061d67bff5848602871f01edff
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2012-10-11 22:07:40 +0200

    Fixed headers.

--- modules/Actors/src/main/java/it/tidalwave/messagebus/impl/netbeans/NetBeansPlatformMessageBus.java
+++ modules/Actors/src/main/java/it/tidalwave/messagebus/impl/netbeans/NetBeansPlatformMessageBus.java
@@ -1,7 +1,7 @@
 /***********************************************************************************************************************
  *
- * SolidBlue - open source safe data
- * Copyright (C) 2011-2012 by Tidalwave s.a.s. (http://tidalwave.it)
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2012 by Tidalwave s.a.s. (http://tidalwave.it)
  *
  ***********************************************************************************************************************
  *
@@ -16,8 +16,8 @@
  *
  ***********************************************************************************************************************
  *
- * WWW: http://solidblue.tidalwave.it
- * SCM: https://bitbucket.org/tidalwave/solidblue-src
+ * WWW: http://thesefoolishthings.java.net
+ * SCM: https://bitbucket.org/tidalwave/thesefoolishthings-src
  *
  **********************************************************************************************************************/
 package it.tidalwave.messagebus.impl.netbeans;
