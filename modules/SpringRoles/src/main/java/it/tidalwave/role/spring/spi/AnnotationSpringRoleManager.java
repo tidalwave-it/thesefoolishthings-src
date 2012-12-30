44138150774e793cf2123a9eae0ebf18101ecf54
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2012-12-30 22:11:19 +0100

    Partial fix for THESEFOOLISHTHINGS-94.

---
+++ modules/SpringRoles/src/main/java/it/tidalwave/role/spring/spi/AnnotationSpringRoleManager.java
@@ -0,0 +1,195 @@
+/***********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2012 by Tidalwave s.a.s. (http://tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.java.net
+ * SCM: https://bitbucket.org/tidalwave/thesefoolishthings-src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.role.spring.spi;
+
+import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.beans.factory.annotation.Configurable;
+import it.tidalwave.util.spring.ClassScanner;
+import it.tidalwave.role.annotation.RoleImplementation;
+import it.tidalwave.role.spi.RoleManager;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+@Configurable @Slf4j
+public class AnnotationSpringRoleManager implements RoleManager
+  {
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @RequiredArgsConstructor @Getter @ToString @EqualsAndHashCode @Slf4j
+    static class ClassAndRole
+      {
+        @Nonnull
+        private final Class<?> ownerClass;
+        
+        @Nonnull
+        private final Class<?> roleClass;
+        
+        @Nonnull
+        public List<ClassAndRole> getSuper()
+          {
+            final List<ClassAndRole> result = new ArrayList<ClassAndRole>();
+            result.add(this);
+            
+            if (ownerClass.getSuperclass() != null)
+              {
+                result.addAll(new ClassAndRole(ownerClass.getSuperclass(), roleClass).getSuper());
+              }
+            
+            for (final Class<?> interfaceClass : ownerClass.getInterfaces())
+              {
+                result.addAll(new ClassAndRole(interfaceClass, roleClass).getSuper());
+              }
+            
+            return result;
+          }
+      }
+    
+    private MultiValueMap<ClassAndRole, Class<?>> roleMapByOwnerClass = new LinkedMultiValueMap<ClassAndRole, Class<?>>();
+   
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
+    @Override @Nonnull
+    public <RoleType> List<? extends RoleType> findRoles (final @Nonnull Object owner, final @Nonnull Class<RoleType> roleClass)
+      {
+        log.trace("findRoles({}, {})", owner, roleClass);
+        
+        final Class<?> ownerClass = owner.getClass();
+        final List<RoleType> roles = new ArrayList<RoleType>();
+        
+        for (final Class<? extends RoleType> roleImplementationClass : findRoleImplementationsFor(ownerClass, roleClass))
+          {
+            for (final Constructor<?> constructor : roleImplementationClass.getDeclaredConstructors())
+              {
+                final Class<?>[] parameterTypes = constructor.getParameterTypes();
+                
+                if ((parameterTypes.length == 1) && parameterTypes[0].isAssignableFrom(ownerClass))
+                  {
+                    try 
+                      {
+                        roles.add((RoleType)constructor.newInstance(owner));
+                      }
+                    catch (Exception e) 
+                      {
+                        log.error("", e);
+                      }
+                  }
+              }
+          }
+        
+        log.trace(">>>> returning: {}", roles);
+        
+        return roles;
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    private synchronized <RoleType> List<Class<? extends RoleType>> findRoleImplementationsFor (final Class<?> ownerClass, final Class<RoleType> roleClass)
+      {
+        final ClassAndRole classAndRole = new ClassAndRole(ownerClass, roleClass);
+        final List<Class<?>> implementations = roleMapByOwnerClass.get(classAndRole);
+        
+        if (implementations != null)
+          {
+            return (List)implementations;
+          }
+        
+        for (final ClassAndRole classAndRole1 : classAndRole.getSuper())
+          {
+            final List<Class<?>> implementations2 = roleMapByOwnerClass.get(classAndRole1);
+
+            if (implementations2 != null)
+              {
+                for (final Class<?> implementation : implementations2)
+                  {
+                    roleMapByOwnerClass.add(classAndRole, implementation);
+                  }
+                
+                return (List)implementations2;
+              }
+          }
+        
+        return Collections.emptyList();
+      }
+    
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @PostConstruct
+    /* package */ void initialize()
+      {
+        final ClassScanner classScanner = new ClassScanner().withAnnotationFilter(RoleImplementation.class);
+        
+        for (final Class<?> roleImplementationClass : classScanner.findClasses())
+          {
+            final RoleImplementation role = roleImplementationClass.getAnnotation(RoleImplementation.class);
+            final Class<?> ownerClass = role.ownerClass();
+            
+            for (final Class<?> roleClass : roleImplementationClass.getInterfaces())
+              {
+                roleMapByOwnerClass.add(new ClassAndRole(ownerClass, roleClass), roleImplementationClass);
+              }
+          }
+        
+        logRoles();
+      }
+        
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    public void logRoles()
+      {
+        log.debug("Configured roles:");
+        
+        for (final Entry<ClassAndRole, List<Class<?>>> entry : roleMapByOwnerClass.entrySet())
+          {
+            log.debug(">>>> {} -> {}", entry.getKey(), entry.getValue());
+          }
+      }
+  }
---
+++ modules/SpringRoles/src/main/java/it/tidalwave/util/spring/ClassScanner.java
@@ -0,0 +1,99 @@
+/***********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * Copyright (C) 2009-2012 by Tidalwave s.a.s. (http://tidalwave.it)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * WWW: http://thesefoolishthings.java.net
+ * SCM: https://bitbucket.org/tidalwave/thesefoolishthings-src
+ *
+ **********************************************************************************************************************/
+package it.tidalwave.util.spring;
+
+import java.lang.annotation.Annotation;
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.util.ClassUtils;
+
+/***********************************************************************************************************************
+ *
+ * A utility for scanning classes in the classpath with some criteria.
+ * 
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class ClassScanner 
+  {
+    private final String basePackages = System.getProperty(ClassScanner.class.getCanonicalName() + ".basePackages", "com:org:it"); // FIME
+    
+    private final ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+
+    /*******************************************************************************************************************
+     *
+     * Scans for classes and returns them.
+     * 
+     * @return  the collection of scanned classes
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public final Collection<Class<?>> findClasses() 
+      {
+        final List<Class<?>> classes = new ArrayList<Class<?>>();
+      
+        for (final String basePackage : basePackages.split(":"))
+          {
+            for (final BeanDefinition candidate : scanner.findCandidateComponents(basePackage)) 
+              {
+                classes.add(ClassUtils.resolveClassName(candidate.getBeanClassName(), ClassUtils.getDefaultClassLoader()));
+              }
+          }
+        
+        return classes;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * Adds an "include" filter.
+     * 
+     * @param  filter  the filter
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public ClassScanner withIncludeFilter (final @Nonnull TypeFilter filter)
+      {
+        scanner.addIncludeFilter(filter);
+        return this;
+      }
+    
+    /*******************************************************************************************************************
+     *
+     * Adds a filter for an annotation.
+     * 
+     * @param  annotationClass  the annotation class
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public ClassScanner withAnnotationFilter (final @Nonnull Class<? extends Annotation> annotationClass)
+      {
+        return withIncludeFilter(new AnnotationTypeFilter(annotationClass));
+      }
+  }
