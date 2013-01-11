715e24c6caaa3442af46fc748f61d99cfdf86132
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-01-11 13:23:11 +0100

    Clean up.

--- modules/examples/AsExample1/src/main/java/it/tidalwave/thesefoolishthings/examples/person/PersonXStreamMarshallable.java
+++ modules/examples/AsExample1/src/main/java/it/tidalwave/thesefoolishthings/examples/person/PersonXStreamMarshallable.java
@@ -25,7 +25,6 @@ package it.tidalwave.thesefoolishthings.examples.person;
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.OutputStream;
-import com.thoughtworks.xstream.XStream;
 import it.tidalwave.role.Marshallable;
 import it.tidalwave.dci.annotation.DciRole;
 import lombok.RequiredArgsConstructor;
