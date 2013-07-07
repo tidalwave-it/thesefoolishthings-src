b058cc26af88b435fbfd488b38b421562cfb3fbd
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-07-07 14:04:26 +0200

    Commented.

--- modules/SpringRoles/src/main/java/it/tidalwave/dci/annotation/DciRole.java
+++ modules/SpringRoles/src/main/java/it/tidalwave/dci/annotation/DciRole.java
@@ -1,27 +1,27 @@
 /*
  * #%L
  * *********************************************************************************************************************
- * 
+ *
  * These Foolish Things - Miscellaneous utilities
  * http://thesefoolishthings.java.net - hg clone https://bitbucket.org/tidalwave/thesefoolishthings-src
  * %%
  * Copyright (C) 2009 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  * *********************************************************************************************************************
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * *********************************************************************************************************************
- * 
+ *
  * $Id$
- * 
+ *
  * *********************************************************************************************************************
  * #L%
  */
@@ -54,8 +54,19 @@ public @interface DciRole
       {
       }
 
+    /*******************************************************************************************************************
+     *
+     * The datum type to which this role can be associated. 
+     *
+     ******************************************************************************************************************/
     @Nonnull
     public Class<?> datum();
 
+    /*******************************************************************************************************************
+     *
+     * The context type to which this role is restricted. If no context is specified, this role will be always
+     * associated.
+     *
+     ******************************************************************************************************************/
     public Class<?> context() default NoContext.class;
   }
