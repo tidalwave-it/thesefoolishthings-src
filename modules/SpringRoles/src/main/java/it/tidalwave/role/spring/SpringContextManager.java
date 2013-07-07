c878eeca8952fbf2863816b1f4e8d0deb3268101
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2013-07-07 14:05:58 +0200

    Commented.

--- modules/SpringRoles/src/main/java/it/tidalwave/role/spring/SpringContextManager.java
+++ modules/SpringRoles/src/main/java/it/tidalwave/role/spring/SpringContextManager.java
@@ -1,27 +1,27 @@
 /*
  * #%L
  * *********************************************************************************************************************
- * 
+ *
  * These Foolish Things - Miscellaneous utilities
  * http://thesefoolishthings.java.net - hg clone https://bitbucket.org/tidalwave/thesefoolishthings-src
  * %%
  * Copyright (C) 2009 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  * *********************************************************************************************************************
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * *********************************************************************************************************************
- * 
+ *
  * $Id$
- * 
+ *
  * *********************************************************************************************************************
  * #L%
  */
@@ -57,6 +57,11 @@ public class SpringContextManager implements ContextManager
           }
       };
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override @Nonnull
     public List<Object> getContexts()
       {
@@ -67,6 +72,11 @@ public class SpringContextManager implements ContextManager
         return contexts;
       }
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override @Nonnull
     public <T> T findContext (final @Nonnull Class<T> contextClass)
       throws NotFoundException
@@ -82,24 +92,44 @@ public class SpringContextManager implements ContextManager
         throw new NotFoundException();
       }
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override
     public void addGlobalContext (final @Nonnull Object context)
       {
         globalContexts.add(context);
       }
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override
     public void addLocalContext (final @Nonnull Object context)
       {
         localContexts.get().push(context);
       }
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override
     public void removeLocalContext (final @Nonnull Object context)
       {
         localContexts.get().remove(context);
       }
 
+    /*******************************************************************************************************************
+     *
+     * {@inheritDoc}
+     *
+     ******************************************************************************************************************/
     @Override @Nonnull
     public <V, T extends Throwable> V runWithContext (final @Nonnull Object context,
                                                       final @Nonnull Task<V, T> task)
