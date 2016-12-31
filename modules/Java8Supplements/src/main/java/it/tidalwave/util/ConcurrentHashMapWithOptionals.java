3ca53b6e347d46f18a4ffc98cfdc5e2df3ee059c
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2016-12-31 12:18:22 +0100

    Fix for TFT-182.

---
+++ modules/Java8Supplements/src/main/java/it/tidalwave/util/ConcurrentHashMapWithOptionals.java
@@ -0,0 +1,85 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * http://thesefoolishthings.tidalwave.it - git clone git@bitbucket.org:tidalwave/thesefoolishthings-src.git
+ * %%
+ * Copyright (C) 2009 - 2016 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.util;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+/***********************************************************************************************************************
+ *
+ * A specialisation of {@link ConcurrentHashMap} with {@link Optional} support.
+ * 
+ * @param <K>   the type of the key
+ * @param <V>   the type of the value
+ *
+ * @since   3.1-ALPHA-2
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class ConcurrentHashMapWithOptionals<K, V> extends ConcurrentHashMap<K, V>
+  {
+    private static final long serialVersionUID = -1771492882183193296L;
+
+    /*******************************************************************************************************************
+     *
+     * If the map doesn't contain the given key, put the new pair(key, value), and return the key itself. Otherwise,
+     * do nothing and return an empty {@link Optional}. The map manipulation is atomically performed by calling
+     * {@link #putIfAbsent(java.lang.Object, java.lang.Object)}.
+     *
+     * If {@code optionalKey} is empty, do nothing and return nothing.
+     *
+     * @param   optionalKey     the key
+     * @param   value           the value
+     * @return                  the new key, or nothing
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public Optional<K> putIfAbsentAndGetNewKey (final @Nonnull Optional<K> optionalKey, final @Nonnull V value)
+      {
+        return optionalKey.flatMap(key -> putIfAbsentAndGetNewKey(key, value));
+      }
+
+    /*******************************************************************************************************************
+     *
+     * If the map doesn't contain the given key, put the new pair(key, value), and return the key itself. Otherwise,
+     * do nothing and return an empty {@link Optional}. The map manipulation is atomically performed by calling
+     * {@link #putIfAbsent(java.lang.Object, java.lang.Object)}.
+     *
+     * @param   key     the key
+     * @param   value   the value
+     * @return          the new key, or nothing
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public Optional<K> putIfAbsentAndGetNewKey (final @Nonnull K key, final @Nonnull V value)
+      {
+        return (putIfAbsent(key, value) == null) ? Optional.of(key) : Optional.empty();
+      }
+  }
