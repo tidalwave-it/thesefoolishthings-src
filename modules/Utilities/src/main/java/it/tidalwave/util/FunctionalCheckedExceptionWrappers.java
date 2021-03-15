ea5631c9d6c126e5903e9bf63964fe58f712dd90
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2021-03-15 12:43:18 +0100

    Fix for TFT-209.

---
+++ modules/Utilities/src/main/java/it/tidalwave/util/FunctionalCheckedExceptionWrappers.java
@@ -0,0 +1,269 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * http://thesefoolishthings.tidalwave.it - git clone git@bitbucket.org:tidalwave/thesefoolishthings-src.git
+ * %%
+ * Copyright (C) 2009 - 2021 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.util;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import lombok.NoArgsConstructor;
+import static lombok.AccessLevel.PRIVATE;
+
+/***********************************************************************************************************************
+ *
+ * A collections of utility methods for simplifying the syntax of lambda expressions with APIs that don't accept
+ * checked exceptions (such as {@link java.util.stream.Stream}): they provide wrapped functions that have no checked
+ * exception in the signature and whose implementation delegates to the original function wrapping an eventual checked
+ * exception into a {@link RuntimeException}. For instance, given the following method that could not be used as a
+ * {@link java.util.stream.Stream#filter(Predicate)} argument:
+ *
+ * <pre>
+ *   private boolean matchEven (final int number)
+ *           throws Exception
+ *     {
+ *       if (number == 13)
+ *         {
+ *           throw new Exception("13!");
+ *         }
+ *
+ *       return number % 2 == 0;
+ *     }
+ * </pre>
+ *
+ * working code can be written as:
+ *
+ * <pre>
+ *   try
+ *     {
+ *       List<Integer> numbers = IntStream.rangeClosed(1, 20)
+ *                                        .mapToObj(Integer::valueOf)
+ *                                        .filter(_p(this::matchEven)) // note the wrapper here
+ *                                        .collect(Collectors.toList());
+ *       ...
+ *     }
+ *   catch (RuntimeException e)
+ *     {
+ *       ...
+ *     }
+ * </pre>
+ *
+ * Any checked exception is wrapped by a {@code RuntimeException}, but {@link IOException} is wrapped by a
+ * {@link UncheckedIOException}.
+ *
+ * @author Fabrizio Giudici
+ * @it.tidalwave.javadoc.draft
+ * @since 3.2-ALPHA-1
+ *
+ **********************************************************************************************************************/
+@NoArgsConstructor(access = PRIVATE)
+public final class FunctionalCheckedExceptionWrappers
+  {
+    /*******************************************************************************************************************
+     *
+     * A variant of {@link Function} that might throw an {@link Exception}. This interface must not be directly used,
+     * it's defined to let the compiler infer functional equivalence.
+     *
+     * @param <T>         the type of the function argument
+     * @param <R>         the type of the function return value
+     *
+     ******************************************************************************************************************/
+    @FunctionalInterface
+    public static interface FunctionWithException<T, R>
+      {
+        public R apply (T t)
+                throws Exception;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A variant of {@link Consumer} that might throw an {@link Exception}. This interface must not be directly used,
+     * it's defined to let the compiler infer functional equivalence.
+     *
+     * @param <T>         the type of the {@code Consumer} argument
+     *
+     ******************************************************************************************************************/
+    @FunctionalInterface
+    public static interface ConsumerWithException<T>
+      {
+        public void accept (T t)
+                throws Exception;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A variant of {@link Supplier} that might throw an {@link Exception}. This interface must not be directly used,
+     * it's defined to let the compiler infer functional equivalence.
+     *
+     * @param <T>         the type of the {@code Supplier} argument
+     *
+     ******************************************************************************************************************/
+    @FunctionalInterface
+    public static interface SupplierWithException<T>
+      {
+        public T get ()
+                throws Exception;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A variant of {@link Predicate} that might throw an {@link Exception}. This interface must not be directly used,
+     * it's defined to let the compiler infer functional equivalence.
+     *
+     * @param <T>         the type of the {@code Predicate} argument
+     *
+     ******************************************************************************************************************/
+    @FunctionalInterface
+    public static interface PredicateWithException<T>
+      {
+        public boolean test (T t)
+                throws Exception;
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A wrapper for a {@link Function} that catches exceptions and wraps them into {@link RuntimeException}s.
+     *
+     * @param function    the {@code Function} to wrap.
+     * @param <T>         the type of the function argument
+     * @param <R>         the type of the function return value
+     * @return            the wrapped {@code Function}
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public static <T, R> Function<T, R> _f (final @Nonnull FunctionWithException<T, R> function)
+      {
+        return t ->
+          {
+            try
+              {
+                return function.apply(t);
+              }
+            catch (Exception e)
+              {
+                throw wrappedException(e);
+              }
+          };
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A wrapper for a {@link Consumer} that catches exceptions and wraps them into {@link RuntimeException}s.
+     *
+     * @param consumer    the {@code Consumer} to wrap.
+     * @param <T>         the type of the {@code Consumer} argument
+     * @return            the wrapped {@code Consumer}
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public static <T> Consumer<T> _c (final @Nonnull ConsumerWithException<T> consumer)
+      {
+        return t ->
+          {
+            try
+              {
+                consumer.accept(t);
+              }
+            catch (Exception e)
+              {
+                throw wrappedException(e);
+              }
+          };
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A wrapper for a {@link Supplier} that catches exceptions and wraps them into {@link RuntimeException}s.
+     *
+     * @param supplier    the {@code Supplier} to wrap.
+     * @param <T>         the type of the {@code Supplier} argument
+     * @return            åthe wrapped {@code Supplier}
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public static <T> Supplier<T> _s (final @Nonnull SupplierWithException<T> supplier)
+      {
+        return () ->
+          {
+            try
+              {
+                return supplier.get();
+              }
+            catch (Exception e)
+              {
+                throw wrappedException(e);
+              }
+          };
+      }
+
+    /*******************************************************************************************************************
+     *
+     * A wrapper for a {@link Predicate} that catches exceptions and wraps them into {@link RuntimeException}s.
+     *
+     * @param predicate   the {@code Predicate} to wrap.
+     * @param <T>         the type of the {@code Predicate} argument
+     * @return            åthe wrapped {@code Predicate}
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    public static <T> Predicate<T> _p (final @Nonnull PredicateWithException<T> predicate)
+      {
+        return t ->
+          {
+            try
+              {
+                return predicate.test(t);
+              }
+            catch (Exception e)
+              {
+                throw wrappedException(e);
+              }
+          };
+      }
+
+    /*******************************************************************************************************************
+     *
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    static RuntimeException wrappedException (Exception e)
+      {
+        if (e instanceof RuntimeException)
+          {
+            return (RuntimeException)e;
+          }
+
+        if (e instanceof IOException)
+          {
+            return new UncheckedIOException((IOException)e);
+          }
+
+        return new RuntimeException(e);
+      }
+  }
---
+++ modules/Utilities/src/test/java/it/tidalwave/util/FunctionalCheckedExceptionWrappersTest.java
@@ -0,0 +1,245 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * http://thesefoolishthings.tidalwave.it - git clone git@bitbucket.org:tidalwave/thesefoolishthings-src.git
+ * %%
+ * Copyright (C) 2009 - 2021 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.util;
+
+import javax.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import static it.tidalwave.util.FunctionalCheckedExceptionWrappers.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Slf4j
+public class FunctionalCheckedExceptionWrappersTest
+  {
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void test_function_wrapper()
+      {
+        final String r = _f(this::sampleFunction).apply("good");
+        assertThat(r, is("good"));
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(expectedExceptions = {RuntimeException.class})
+    public void test_function_wrapper_with_exception()
+      {
+        final String r = _f(this::sampleFunctionWithException).apply("bad");
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void test_consumer_wrapper()
+      {
+        _c(this::sampleConsumer).accept("good");
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(expectedExceptions = {RuntimeException.class})
+    public void test_consumer_wrapper_with_exception()
+      {
+        _c(this::sampleConsumerWithException).accept("foo");
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void test_supplier_wrapper()
+      {
+        final String r = _s(this::sampleSupplier).get();
+        assertThat(r, is("foo"));
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(expectedExceptions = {RuntimeException.class})
+    public void test_supplier_wrapper_with_exception()
+      {
+        final String r = _s(this::sampleSupplierWithException).get();
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void test_predicate_wrapper()
+      {
+        final boolean r = _p(this::samplePredicate).test("foo");
+        assertThat(r, is(true));
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(expectedExceptions = {RuntimeException.class})
+    public void test_predicate_wrapper_with_exception()
+      {
+        final boolean r = _p(this::samplePredicateWithException).test("foo");
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test // this is just to see a code example with Streams
+    public void test_with_Stream()
+      {
+        final List<Integer> numbers = IntStream.rangeClosed(1, 10)
+                                               .mapToObj(Integer::valueOf)
+                                               .filter(_p(this::matchEven))
+                                               .collect(Collectors.toList());
+        log.info("Even numbers: {}", numbers);
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test(expectedExceptions = {RuntimeException.class}) // this is just to see a code example with Streams
+    public void test_with_Stream_with_exception()
+      {
+        final List<Integer> numbers = IntStream.rangeClosed(1, 20)
+                                               .mapToObj(Integer::valueOf)
+                                               .filter(_p(this::matchEven))
+                                               .collect(Collectors.toList());
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void must_not_wrap_RuntimeException()
+      {
+        final RuntimeException e = new RuntimeException();
+        final RuntimeException we = wrappedException(e);
+        assertThat(we, is(sameInstance(e)));
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void must_wrap_checked_exceptions()
+      {
+        final InterruptedException e = new InterruptedException();
+        final RuntimeException we = wrappedException(e);
+        assertThat(we.getCause(), is(sameInstance(e)));
+      }
+
+    /*******************************************************************************************************************
+     *
+     ******************************************************************************************************************/
+    @Test
+    public void must_wrap_IOException_with_UncheckedIOException()
+      {
+        final IOException e = new IOException();
+        final RuntimeException we = wrappedException(e);
+        assertThat(we.getCause(), is(sameInstance(e)));
+        assertThat(we, is(instanceOf(UncheckedIOException.class)));
+      }
+
+    /*******************************************************************************************************************
+     *
+     * These can be static, but in this way :: operators above look shorted.
+     *
+     ******************************************************************************************************************/
+    @Nonnull
+    private String sampleFunction (final @Nonnull String arg)
+            throws Exception
+      {
+        return arg;
+      }
+
+    @Nonnull
+    private String sampleFunctionWithException (final @Nonnull String arg)
+            throws Exception
+      {
+        throw new Exception();
+      }
+
+    private void sampleConsumer (final @Nonnull String arg)
+            throws Exception
+      {
+      }
+
+    private void sampleConsumerWithException (final @Nonnull String arg)
+            throws Exception
+      {
+        throw new Exception();
+      }
+
+    @Nonnull
+    private String sampleSupplier()
+            throws Exception
+      {
+        return "foo";
+      }
+
+    @Nonnull
+    private String sampleSupplierWithException()
+            throws Exception
+      {
+        throw new Exception();
+      }
+
+    private boolean samplePredicate (final @Nonnull String arg)
+            throws Exception
+      {
+        return true;
+      }
+
+    private boolean samplePredicateWithException (final @Nonnull String arg)
+            throws Exception
+      {
+        throw new Exception();
+      }
+
+    @Nonnull
+    private boolean matchEven (final int number)
+            throws Exception
+      {
+        if (number == 13)
+          {
+            throw new Exception("13!");
+          }
+
+        return number % 2 == 0;
+      }
+  }
