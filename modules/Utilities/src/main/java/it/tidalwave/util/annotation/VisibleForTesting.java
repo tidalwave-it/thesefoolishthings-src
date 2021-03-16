88bfa176784cb9df1acdb7000da7da762cea472e
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2021-03-16 17:58:04 +0100

    Fix for TFT-211.

---
+++ modules/Utilities/src/main/java/it/tidalwave/util/annotation/VisibleForTesting.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * These Foolish Things - Miscellaneous utilities
+ * http://thesefoolishthings.tidalwave.it - git clone git@bitbucket.org:tidalwave/thesefoolishthings-src.git
+ * %%
+ * Copyright (C) 2009 - 2021 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.util.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.*;
+
+/***********************************************************************************************************************
+ *
+ * A documenting annotation to state that a given element has a less restrictive access modifier (e.g. is not private)
+ * only for testing purposes (i.e. it must be accessed by a test class).
+ *
+ * @author  Fabrizio Giudici
+ * @since 3.2-ALPHA-2
+ * @it.tidalwave.javadoc.stable
+ *
+ **********************************************************************************************************************/
+@Retention(RetentionPolicy.SOURCE) @Documented @Target({TYPE, FIELD, METHOD})
+public @interface VisibleForTesting
+  {
+  }
