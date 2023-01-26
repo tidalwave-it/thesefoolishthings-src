d007e8d1db72b4c8182d1165a75956321b6eceb3
Fabrizio Giudici <fabrizio.giudici@tidalwave.it>
2023-01-26 17:02:23 +0100

    Injection methods added to ReflectionUtils.

---
+++ modules/Utilities/src/test/java/it/tidalwave/util/ReflectionUtilsTest.java
@@ -0,0 +1,130 @@
+/*
+ * *********************************************************************************************************************
+ *
+ * TheseFoolishThings: Miscellaneous utilities
+ * http://tidalwave.it/projects/thesefoolishthings
+ *
+ * Copyright (C) 2009 - 2023 by Tidalwave s.a.s. (http://tidalwave.it)
+ *
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * git clone https://bitbucket.org/tidalwave/thesefoolishthings-src
+ * git clone https://github.com/tidalwave-it/thesefoolishthings-src
+ *
+ * *********************************************************************************************************************
+ */
+package it.tidalwave.util;
+
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+/***********************************************************************************************************************
+ *
+ * @author Fabrizio Giudici
+ *
+ **********************************************************************************************************************/
+public class ReflectionUtilsTest
+  {
+    static class Bean1 {}
+    static class Bean2 {}
+    static class Bean3 {}
+
+    @AllArgsConstructor
+    static class WithConstructor
+      {
+        private final Bean1 bean1;
+        private final Bean2 bean2;
+        private final Bean3 bean3;
+      }
+
+    @Getter
+    static class WithAnnotation
+      {
+        @Inject private Bean1 bean1;
+        @Inject private Bean2 bean2;
+        @Inject private Bean3 bean3;
+      }
+
+    /******************************************************************************************************************/
+    @Test
+    public void test_instantiateWithDependencies()
+      {
+        // given
+        final var beanMap = Map.of(Bean1.class, new Bean1(),
+                                   Bean2.class, new Bean2(),
+                                   Bean3.class, new Bean3());
+        // when
+        final var object = ReflectionUtils.instantiateWithDependencies(WithConstructor.class, beanMap);
+        // then
+        // assertThat(object, isNotNull());
+        assertThat(object.bean1, is(sameInstance(beanMap.get(Bean1.class))));
+        assertThat(object.bean2, is(sameInstance(beanMap.get(Bean2.class))));
+        assertThat(object.bean3, is(sameInstance(beanMap.get(Bean3.class))));
+      }
+
+    /******************************************************************************************************************/
+    @Test
+    public void test_injectDependencies()
+      {
+        // given
+        final var beanMap = Map.of(Bean1.class, new Bean1(),
+                                   Bean2.class, new Bean2(),
+                                   Bean3.class, new Bean3());
+        final var object = new WithAnnotation();
+        // when
+        ReflectionUtils.injectDependencies(object, beanMap);
+        // then
+        assertThat(object.bean1, is(sameInstance(beanMap.get(Bean1.class))));
+        assertThat(object.bean2, is(sameInstance(beanMap.get(Bean2.class))));
+        assertThat(object.bean3, is(sameInstance(beanMap.get(Bean3.class))));
+      }
+
+    /******************************************************************************************************************/
+    @Test(dataProvider = "dataProvider")
+    public void test_getClass (@Nonnull final Type type, @Nonnull final Class<?> expectedResult)
+      {
+        final var actualResult = ReflectionUtils.getClass(type);
+        assertThat(actualResult, is(equalTo(expectedResult)));
+      }
+
+    /******************************************************************************************************************/
+    @DataProvider
+    public static Object[][] dataProvider()
+      {
+        final var genericOfInteger = mock(ParameterizedType.class);
+        when(genericOfInteger.getRawType()).thenReturn(Integer.class);
+
+        final var arrayOfInt = mock(GenericArrayType.class);
+        when(arrayOfInt.getGenericComponentType()).thenReturn(int.class);
+
+        return new Object[][]
+          {
+            { String.class, String.class },
+            { String[].class, String[].class },
+            { genericOfInteger, Integer.class},
+            { arrayOfInt, int[].class }
+          };
+      }
+  }
